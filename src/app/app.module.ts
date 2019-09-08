ac6d8d0a7db33b4c167a1e94f1f4e965ebcf7e98
Borys Tymiński <tyminski.borys@gmail.com>
2019-09-08 17:02:11 +0200

    add dark mode

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 // light mode
+<<<<<<< Updated upstream
 import { BgTopComponent } from './light-mode/top/bg-top/bg-top.component';
 import { SkillsComponent } from './light-mode/skills/skills.component';
 import { PortfolioComponent } from './light-mode/portfolio/portfolio.component';
@@ -18,9 +19,30 @@ import { FooterComponent } from './light-mode/footer/footer.component';
 import { TopComponent } from './light-mode/top/top.component';
 import { LightModeComponent } from './light-mode/light-mode.component';
 import { DarkModeComponent } from './dark-mode/dark-mode.component';
+=======
+import { BgTopComponent } from './components/light-mode/top/bg-top/bg-top.component';
+import { SkillsComponent } from './components/light-mode/skills/skills.component';
+import { PortfolioComponent } from './components/light-mode/portfolio/portfolio.component';
+import { ContactComponent } from './components/light-mode/contact/contact.component';
+import { LightNavComponent } from './components/light-mode/top/light-nav/light-nav.component';
+import { GreetingComponent } from './components/light-mode/top/greeting/greeting.component';
+import { LogoComponent } from './components/light-mode/top/logo/logo.component';
+import { FooterComponent } from './components/light-mode/footer/footer.component';
+import { TopComponent } from './components/light-mode/top/top.component';
+import { LightModeComponent } from './components/light-mode/light-mode.component';
+import { DarkModeComponent } from './components/dark-mode/dark-mode.component';
+import { ToolsComponent } from './components/light-mode/top/tools/tools.component';
+import { FlagsComponent } from './components/light-mode/top/tools/flags/flags.component';
+import { ModesComponent } from './components/light-mode/top/tools/modes/modes.component';
+>>>>>>> Stashed changes
 
+//dark mode
+import { FollowerComponent } from './components/dark-mode/follower/follower.component';
+import { DarkNavComponent } from './components/dark-mode/dark-nav/dark-nav.component';
+import { PreviewComponent } from './components/dark-mode/preview/preview.component';
 
 @NgModule({
+<<<<<<< Updated upstream
   declarations: [
     AppComponent,
     BgTopComponent,
@@ -43,5 +65,37 @@ import { DarkModeComponent } from './dark-mode/dark-mode.component';
   ],
   providers: [],
   bootstrap: [AppComponent]
+=======
+    declarations: [
+        AppComponent,
+        // light mode
+        BgTopComponent,
+        SkillsComponent,
+        PortfolioComponent,
+        ContactComponent,
+        LightNavComponent,
+        GreetingComponent,
+        LogoComponent,
+        FooterComponent,
+        TopComponent,
+        LightModeComponent,
+        DarkModeComponent,
+        ToolsComponent,
+        FlagsComponent,
+        ModesComponent,
+        // dark mode
+        FollowerComponent,
+        DarkNavComponent,
+        PreviewComponent
+    ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        FormsModule,
+        HttpClientModule
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
+>>>>>>> Stashed changes
 })
 export class AppModule { }
---
+++ src/app/dark-mode/follower/follower.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit, HostListener } from '@angular/core';
+import {TouchService} from "../../services/touch.service";
+
+@Component({
+    selector: 'app-follower',
+    templateUrl: './follower.component.html',
+    styleUrls: ['./follower.component.scss']
+})
+export class FollowerComponent implements OnInit {
+    topPos: string;
+    leftPos: string;
+    holding: boolean;
+
+    @HostListener('document:mousemove', ['$event'])
+    onMouseMove(e) {
+        this.topPos = e.clientY + 'px';
+        this.leftPos = e.clientX + 'px';
+    }
+
+    constructor(private touchService: TouchService) {
+        this.touchService.touch$.subscribe(e => this.holding = e);
+    }
+
+    ngOnInit() {
+    }
+
+}
