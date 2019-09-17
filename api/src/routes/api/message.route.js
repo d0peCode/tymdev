245be025fef112ba73aa0e4432679996d90daac3
Borys Tymiński <tyminski.borys@gmail.com>
2019-09-17 13:22:36 +0200

    fix message delete endpoint

--- api/src/routes/api/message.route.js
+++ api/src/routes/api/message.route.js
@@ -8,6 +8,6 @@ const messageController = require('../../controllers/message.controller')
 // Authentication example routes
 router.post('/add', messageController.add)
 router.get('/get', auth(['admin']), messageController.get)
-router.post('/remove', auth(['admin']), messageController.add)
+router.delete('/remove', auth(['admin']), messageController.remove)
 
 module.exports = router
