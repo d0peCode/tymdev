eec37ce7dd41a7a289949640ec386ee4e8b20887
Borys Tymiński <tyminski.borys@gmail.com>
2019-10-23 03:09:35 +0200

    add changing language functionality in flags.component

--- public/src/app/components/app.component.ts
+++ public/src/app/components/app.component.ts
@@ -5,15 +5,11 @@ import { TranslateService } from '@ngx-translate/core';
     selector: 'app-root',
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.scss'],
+    providers: [TranslateService]
 })
 export class AppComponent  {
-    title = 'tymdev-new';
-
-  constructor(translate: TranslateService) {
-    // this language will be used as a fallback when a translation isn't found in the current language
+  constructor(private translate: TranslateService) {
     translate.setDefaultLang('en');
-
-    // the lang to use, if the lang isn't available, it will use the current loader to get them
-    translate.use('en');
+    translate.use('pl');
   }
 }
